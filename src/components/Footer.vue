2dcf33bf638596228a38a73d3994cf715d3ed1fc
Nino Popa <nino@gridbase.studio>
2021-05-11 22:30:46 +0300

    completed mobile footer design

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -16,7 +16,11 @@
             class="footer-logo-image"
           />
           <div class="back-home-link-text-and-arrow">
-            <g-image class="small-link-arrow" src="~/images/next.svg" />
+            <g-image
+              class="small-link-arrow"
+              src="~/images/next.svg"
+              alt="small link arrow"
+            />
             <h6 class="small-link-text">BACK HOME</h6>
           </div>
         </g-link>
@@ -27,7 +31,54 @@
       </div>
     </div>
     <div class="design-development-credits">
-      <h2 class="credit-title">Design & Development Credits</h2>
+      <h2 class="credits-title">Design & Development Credits</h2>
+      <p class="credits-paragraph">
+        Visual narrative & design concepts<br />
+        by
+        <span class="cormorand-italic">founder Nino.P</span>
+      </p>
+      <p class="credits-paragraph">
+        Development & implmentation architecture<br />
+        by
+        <span class="cormorand-italic">gridbase® studio</span>
+      </p>
+      <p class="credits-paragraph">
+        Photography by <span class="cormorand-italic">Paweł Czerwiński</span>
+      </p>
+      <g-link to="/work/" class="footer-arrow-link">
+        <span class="footer-link-text">see our work</span>
+        <g-image
+          src="~/images/curved-arrow.svg"
+          alt="link-arrow"
+          class="button-link-arrow"
+        />
+      </g-link>
+    </div>
+    <div class="general-enquiries">
+      <h2 class="enquries-title">
+        General Enquiries<br />
+        & Business Info
+      </h2>
+      <g-link to="/contact/" class="footer-arrow-link">
+        <span class="footer-link-text">contact us</span>
+        <g-image
+          src="~/images/curved-arrow.svg"
+          alt="link-arrow"
+          class="button-link-arrow"
+        />
+      </g-link>
+      <h5 class="direct-option-text">Or</h5>
+      <p class="direct-email-options">
+        Don’t fancy contact forms ?<br />
+        Say hello! → nino@gridbase.studio
+      </p>
+    </div>
+    <div class="gradient-footer-end">
+      <h3 class="copyright">©2021 - gridbase® studio</h3>
+      <div class="legal-link-container">
+        <g-link class="legal-link">Privacy Policy<br />↗︎</g-link>
+        <g-link class="legal-link">Terms & Conditions<br />↗︎</g-link>
+      </div>
     </div>
   </footer>
 </template>
@@ -156,9 +207,99 @@ body {
     display: flex;
     flex-direction: column;
     align-items: center;
-    .credit-title {
+    padding-bottom: 55px;
+    border-bottom: 1px solid $nav-line;
+    .credits-title {
       text-align: center;
       max-width: 70vw;
+    }
+    .credits-paragraph {
+      font-family: "Questrial";
+      font-size: 1em;
+      text-align: center;
+      .cormorand-italic {
+        font-family: "Cormorant Garamond";
+        font-style: italic;
+        font-size: 1.38em;
+      }
+    }
+    .footer-arrow-link {
+      margin-top: 50px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      text-decoration: none;
+      color: $grid-black;
+      .footer-link-text {
+        font-size: 1.4em;
+      }
+    }
+  }
+  .general-enquiries {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 120px;
+    padding-bottom: 30px;
+    .enquries-title {
+      max-width: 70vw;
+      text-align: center;
+    }
+    .footer-arrow-link {
+      margin-top: 50px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      text-decoration: none;
+      color: $grid-black;
+      .footer-link-text {
+        font-size: 1.4em;
+      }
+    }
+    .direct-option-text {
+      font-size: 1em;
+      margin-top: 55px;
+      margin-bottom: 20px;
+    }
+    .direct-email-options {
+      font-size: 1em;
+      text-align: center;
+    }
+  }
+  .gradient-footer-end {
+    max-width: 100vw;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    min-height: 202px;
+    padding: 0 19px 22px 19px;
+    background-image: linear-gradient(
+      180deg,
+      rgba(255, 255, 255, 0) 0%,
+      rgba(186, 186, 186, 0.2) 17%,
+      rgba(118, 118, 118, 0.42) 39%,
+      rgba(83, 83, 83, 0.67) 59%,
+      rgba(70, 70, 70, 0.75) 66%,
+      rgba(54, 54, 54, 0.82) 76%,
+      #1a1a1a 100%
+    );
+    .copyright {
+      font-size: 1.3em;
+      text-decoration: none;
+      position: relative;
+      top: 52px;
+      font-weight: 400;
+      color: #fff;
+      margin-bottom: 10px;
+    }
+    .legal-link-container {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+    }
+    .legal-link {
+      color: #fff;
+      text-decoration: none;
     }
   }
 }
